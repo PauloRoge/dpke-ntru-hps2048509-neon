d206df1f490092981085994a43af1314be988f87
Paulo R. A. Candido Jr <paulocandido815@gmail.com>
2026-03-05 00:20:13 -0300

    Multiplicação polinomial por convolução

--- src/binary_inversion.c
+++ src/binary_inversion.c
@@ -1,55 +1,38 @@
 #include "binary_inversion.h"
-
 #include <arm_neon.h>
+
+#define NWORDS 2 
 
 poly128_t binary_polynomial_multiplication_64x64_to_128(poly64_t a, poly64_t b) {
     // Perform polynomial multiplication using NEON intrinsics
     return vmull_p64(a, b);
 }
 
-void binary_polynomial_multiplication_128x128_to_256(const poly64_t a[2],
-    const poly64_t b[2], poly128_t c[2]) {
-    const poly64_t a0 = a[0];
-    const poly64_t a1 = a[1];
-    const poly64_t b0 = b[0];
-    const poly64_t b1 = b[1];
+void binary_polynomial_multiplication_128x128_to_256(const poly64_t a[NWORDS], const poly64_t b[NWORDS],
+    poly128_t c[NWORDS]) {
 
-    // (aL + x^64*aH)
-    const poly128_t p00 = vmull_p64(a0, b0); // a0*b0
-    const poly128_t p01 = vmull_p64(a0, b1); // a0*b1
-    const poly128_t p10 = vmull_p64(a1, b0); // a1*b0
-    const poly128_t p11 = vmull_p64(a1, b1); // a1*b1
+    // inicializa para resultado
+    uint64_t out_words[2*NWORDS] = {0};
 
-    // aL, aH
-    const uint64x2_t u00 = vreinterpretq_u64_p128(p00);
-    const uint64x2_t u01 = vreinterpretq_u64_p128(p01);
-    const uint64x2_t u10 = vreinterpretq_u64_p128(p10);
-    const uint64x2_t u11 = vreinterpretq_u64_p128(p11);
+    for (int i = 0; i < NWORDS; i++) {
+        for (int j = 0; j < NWORDS; j++) {
 
-    const uint64_t p00_lo = vgetq_lane_u64(u00, 0);
-    const uint64_t p00_hi = vgetq_lane_u64(u00, 1);
+            poly128_t p = vmull_p64(a[i], b[j]);
 
-    const uint64_t p01_lo = vgetq_lane_u64(u01, 0);
-    const uint64_t p01_hi = vgetq_lane_u64(u01, 1);
+            uint64x2_t u = vreinterpretq_u64_p128(p);
+            uint64_t p_lo = vgetq_lane_u64(u, 0);
+            uint64_t p_hi = vgetq_lane_u64(u, 1);
 
-    const uint64_t p10_lo = vgetq_lane_u64(u10, 0);
-    const uint64_t p10_hi = vgetq_lane_u64(u10, 1);
+            int k = i+j;
+            out_words[k] ^= p_lo;
+            out_words[k + 1] ^= p_hi;
+        }
+    }
 
-    const uint64_t p11_lo = vgetq_lane_u64(u11, 0);
-    const uint64_t p11_hi = vgetq_lane_u64(u11, 1);
-
-    const uint64_t s_lo = p01_lo ^ p10_lo;
-    const uint64_t s_hi = p01_hi ^ p10_hi;
-
-    uint64x2_t c0 = vdupq_n_u64(0);
-    uint64x2_t c1 = vdupq_n_u64(0);
-
-    c0 = vsetq_lane_u64(p00_lo, c0, 0);
-    c0 = vsetq_lane_u64(p00_hi ^ s_lo, c0, 1);
-
-    c1 = vsetq_lane_u64(s_hi ^ p11_lo, c1, 0);
-    c1 = vsetq_lane_u64(p11_hi, c1, 1);
-
-    c[0] = vreinterpretq_p128_u64(c0);
-    c[1] = vreinterpretq_p128_u64(c1);
+    for (int t = 0; t < NWORDS; t++) {
+        uint64x2_t v = vdupq_n_u64(0);
+        v = vsetq_lane_u64(out_words[2*t + 0], v, 0);
+        v = vsetq_lane_u64(out_words[2*t + 1], v, 1);
+        c[t] = vreinterpretq_p128_u64(v);
+    }
 }
--- src/binary_inversion.h
+++ src/binary_inversion.h
@@ -1,11 +1,12 @@
 #ifndef BINARY_INVERSION_H
 #define BINARY_INVERSION_H
+#define NWORDS 2 
 
 #include <arm_neon.h>
 
 poly128_t binary_polynomial_multiplication_64x64_to_128(poly64_t a, poly64_t b);
 
-void binary_polynomial_multiplication_128x128_to_256(const poly64_t a[2], const poly64_t b[2],
-    poly128_t c[2]);
+void binary_polynomial_multiplication_128x128_to_256(const poly64_t a[NWORDS], const poly64_t b[NWORDS],
+    poly128_t c[NWORDS]);
     
 #endif // BINARY_INVERSION_H
