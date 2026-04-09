b8e96a5103c9d8d6e7c51276d966bb1793460482
Paulo R. A. Candido Jr <paulocandido815@gmail.com>
2026-04-09 11:13:03 -0300

    Função multi-digito para operação XOR

--- src/binary_inversion.h
+++ src/binary_inversion.h
@@ -21,8 +21,6 @@ void binary_polynomial_multiplication_512x512_to_1024(poly64_t a[8], poly64_t b[
 
 void mul_karatsuba(poly64_t a[2], poly64_t b[2], poly128_t c[2]);
 
-void mul_karatsuba128x128(poly64_t a[2], poly64_t b[2], poly128_t c[2]);
-
 void mul_karatsuba256x256(poly64_t a[4], poly64_t b[4], poly128_t c[4]);
 
 void mul_karatsuba512x512(poly64_t a[8], poly64_t b[8], poly128_t c[8]);
